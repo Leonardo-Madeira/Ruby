0769e31b06c62d8ea6e4fd2ac0fd68ec037000d2
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-04 18:04:42 -0300

    :loop:

---
+++ fundamentals/loop/11-for.rb
@@ -0,0 +1,8 @@
+food = ['Açaí','Carne','Frango', 'Peixe']
+
+user = {nome: 'Leonardo', idade: 25, altura: 1.69, peso:71, endereco:{cidade:'Florianópolis', estado: 'SC', bairro: 'Centro'}}
+
+
+for key, value in user do
+    puts "#{key} = #{value}"
+end
---
+++ fundamentals/loop/12-while.rb
@@ -0,0 +1,8 @@
+food = ['Açaí','Carne','Frango', 'Peixe']
+
+count = 0
+
+while count < food.size do
+    puts "#{count} #{food[count]}"
+    count +=1
+end
