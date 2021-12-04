8809541872ea6f507f0be8c0110bb354b7ed2ccf
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-04 18:03:47 -0300

    :eyes:  simbolo

---
+++ fundamentals/09-simbolo.rb
@@ -0,0 +1,10 @@
+# cria apenas um id
+puts :name.object_id
+puts :name.object_id
+
+# cria outro id 
+puts "name".object_id
+puts "name".object_id
+
+hashes = {nome1: "Leonardo", idade1:25,nome2: "Dayane", idade2:30}
+puts hashes
