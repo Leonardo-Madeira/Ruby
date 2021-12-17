59e5e033a63929bc28d15cf0881d2804a6132859
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-16 23:23:06 -0300

    Update 11-for.rb

--- fundamentals/loop/11-for.rb
+++ fundamentals/loop/11-for.rb
@@ -1,7 +1,5 @@
 food = ['Açaí','Carne','Frango', 'Peixe']
-
 user = {nome: 'Leonardo', idade: 25, altura: 1.69, peso:71, endereco:{cidade:'Florianópolis', estado: 'SC', bairro: 'Centro'}}
-
 
 for key, value in user do
     puts "#{key} = #{value}"
